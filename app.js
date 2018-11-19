e3a45c2f6cbe583b6ffd9c766ef862b34dd111a5
Jon Gomez <jongomez@jgomezmbpro.local>
2018-11-19 11:57:18 -0500

    Venue bouncer quiz

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-let age = prompt("How old are you?")
-alert("You are approximately " + age*365 + " days old.")
+let age = prompt("How old are you?");
+
+if (age < 18) {
+  alert("You cannot enter the venue.")
+} else if (age < 21) {
+  alert("You can enter, but cannot drink.")
+} else {
+  alert("You can enter and drink.")
+}
+
+if (age < 0) {
+  alert("Error: You haven't even been born yet.")
+}
+
+if (age == 21) {
+  alert("Happy 21st Birthday!!")
+}
+
+if (age % 2 === 1) {
+  alert("Your age is odd!")
+}
