e4a87294e4805eb3bb3c97f6b3724379dca85b55
Jon Gomez <jongomez@jgomezmbpro.local>
2018-11-19 16:21:43 -0500

    For Loops Problem Set

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-console.log("Print odd numbers between 300 and 333.");
-let num = 300;
+console.log("Print all numbers between -10 and 19.");
+for (let i = -10; i <= 19; i++) {
+  console.log(i);
+}
 
-while (num <= 333) {
-  if (num % 2 === 1) {
-    console.log(num);
+console.log("Print all even numbers between 10 and 40.");
+for (let i = 11; i <= 40; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
   }
-  num++;
+}
+
+console.log("Print odd numbers between 300 and 333.");
+for (let i = 300; i <= 333; i++) {
+  if (i % 2 === 1) {
+    console.log(i);
+  }
 }
 
 console.log("Print numbers divisible by 5 and 3 between 5 and 50.");
-let num2 = 5;
-
-while (num2 <= 50) {
-  if ((num2 % 5 === 0) && (num2 % 3 === 0)) {
-    console.log(num2);
+for (let i = 5; i <= 50; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log(i);
   }
-  num2++
 }
