19864cb04624e3ddde2d7fd755939060b696e694
Jon Gomez <jongomez@jgomezmbpro.local>
2018-11-20 09:49:26 -0500

    Functions quiz

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-console.log("Print all numbers between -10 and 19.");
-for (let i = -10; i <= 19; i++) {
-  console.log(i);
+function isEven(num) {
+  return num % 2 === 0;
 }
 
-console.log("Print all even numbers between 10 and 40.");
-for (let i = 11; i <= 40; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    let solution = 1;
+    for (let i = 2; i <= num; i++) {
+      solution *= i;
+    }
+    return solution;
   }
 }
-
-console.log("Print odd numbers between 300 and 333.");
-for (let i = 300; i <= 333; i++) {
-  if (i % 2 === 1) {
-    console.log(i);
-  }
-}
-
-console.log("Print numbers divisible by 5 and 3 between 5 and 50.");
-for (let i = 5; i <= 50; i++) {
-  if (i % 5 === 0 && i % 3 === 0) {
-    console.log(i);
-  }
+function kebabToSnake(string) {
+  let newStr = string.replace(/-/g, "_");
+  return newStr
 }
