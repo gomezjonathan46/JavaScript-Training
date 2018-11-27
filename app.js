dbbf76abee9e25210869e4f8a9eb47817c0c9900
Jon Gomez <jongomez@jgomezmbpro.local>
2018-11-27 12:56:16 -0500

    Set constant for button

---
+++ app.js
@@ -0,0 +1,4 @@
+// Buttons
+const playerOne = document.getElementsByTagName('button')[0];
+const playerTwo = document.getElementsByTagName('button')[1];
+const reset = document.getElementsByTagName('button')[2];
