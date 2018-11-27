81b291c310c800948b757556445873186cc649f3
Jon Gomez <jongomez@jgomezmbpro.local>
2018-11-27 13:01:12 -0500

    Set const for player scores

--- app.js
+++ app.js
@@ -2,3 +2,7 @@
 const playerOne = document.getElementsByTagName('button')[0];
 const playerTwo = document.getElementsByTagName('button')[1];
 const reset = document.getElementsByTagName('button')[2];
+
+// Score
+const playerOneScore = document.getElementsByClassName('player')[0];
+const playerTwoScore = document.getElementsByClassName('player')[1];
