bdcdcfa868f2a103f29462350af8f1b9541f1bed
Jon Gomez <jongomez@jgomezmbpro.local>
2018-11-27 13:03:58 -0500

    Set max score

--- app.js
+++ app.js
@@ -6,3 +6,4 @@ const reset = document.getElementsByTagName('button')[2];
 // Score
 const playerOneScore = document.getElementsByClassName('player')[0];
 const playerTwoScore = document.getElementsByClassName('player')[1];
+const maxScore = document.querySelector(".maxScore");
