6c562313993e2e87e3bda9f85ff1c265592c567e
Jon Gomez <jongomez@breedmbpro.brownsteingroup.com>
2018-12-13 17:25:26 -0500

    Sorted? yes? no? how? challenge

--- app.js
+++ app.js
@@ -1,35 +1,20 @@
-function dirReduc(arr) {
-  console.log(arr);
-  let dirNums = arr.join(',').replace(/NORTH/g, 1).replace(/SOUTH/g, -1).replace(/EAST/g, 2).replace(/WEST/g, -2).split(','), cDirs = Array();
-  for(var i = 0; i < arr.length; i++){
-    if(Number(dirNums[i]) + Number(dirNums[i+1]) != 0 && Number(dirNums[i]) + Number(dirNums[i-1]) != 0){
-      if(Number(cDirs[cDirs.length - 1]) + Number(dirNums[i]) != 0){
-        cDirs.push(dirNums[i]);
+function isSortedAndHow(array) {
+  if (array[0] >= array[1]) {
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] <= array[i-1] === false) {
+        return "no";
       }
-      else cDirs.pop();
     }
+    return "yes, descending";
   }
-  if(cDirs.length) cDirs = cDirs.join(',').replace(/-1/g, 'SOUTH').replace(/1/g, 'NORTH').replace(/-2/g, 'WEST').replace(/2/g, 'EAST').split(',')
-  console.log(cDirs);
-}
-
-let arr = [];
-let directions = ['NORTH','SOUTH','EAST','WEST'];
-let min = 0;
-let max = 4;
-let minArrLength = 7;
-let maxArrLength = 14;
-
-function generateArr() {
-  for (let i = 0; i <= Math.floor(Math.random() * (+maxArrLength - +minArrLength) + +minArrLength); i++) {
-    arr.push(directions[Math.floor(Math.random() * (+max - +min)) + +min]);
+  if (array[0] <= array[1]) {
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] >= array[i-1] === false) {
+        return "no";
+      }
+    }
+    return "yes, ascending";
   }
-  return arr;
+  return "no"
 }
-
-function loop() {
-  for (let i = 0; i <= 2; i++) {
-    arr = [];
-    dirReduc(generateArr());
-  }
-}
+ 
