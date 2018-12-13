9e02aa501418388d4e4d69777fe8d227abaa8a1c
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2018-12-13 00:43:00 -0500

    Autogenerate array and run function

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function dirReduc(arr) {
+  console.log(arr);
   let dirNums = arr.join(',').replace(/NORTH/g, 1).replace(/SOUTH/g, -1).replace(/EAST/g, 2).replace(/WEST/g, -2).split(','), cDirs = Array();
   for(var i = 0; i < arr.length; i++){
     if(Number(dirNums[i]) + Number(dirNums[i+1]) != 0 && Number(dirNums[i]) + Number(dirNums[i-1]) != 0){
@@ -9,21 +10,26 @@ function dirReduc(arr) {
     }
   }
   if(cDirs.length) cDirs = cDirs.join(',').replace(/-1/g, 'SOUTH').replace(/1/g, 'NORTH').replace(/-2/g, 'WEST').replace(/2/g, 'EAST').split(',')
-  return cDirs;
+  console.log(cDirs);
 }
 
 let arr = [];
 let directions = ['NORTH','SOUTH','EAST','WEST'];
-let min=0;
-let max=3;
+let min = 0;
+let max = 4;
 let minArrLength = 7;
-let maxArrLength = 13;
-let randomDir = Math.floor(Math.random() * (+max - +min)) + +min;
-let randomArrLength = Math.floor(Math.random() * (+maxArrLength - +minArrLength) + +minArrLength);
+let maxArrLength = 14;
 
 function generateArr() {
-  for (let i = 0; i <= randomArrLength; i++) {
-    arr[i].push(directions[randomDir]);
+  for (let i = 0; i <= Math.floor(Math.random() * (+maxArrLength - +minArrLength) + +minArrLength); i++) {
+    arr.push(directions[Math.floor(Math.random() * (+max - +min)) + +min]);
   }
   return arr;
+}
+
+function loop() {
+  for (let i = 0; i <= 2; i++) {
+    arr = [];
+    dirReduc(generateArr());
+  }
 }
